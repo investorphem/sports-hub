f3edb39e0610984553586246fac8d091c18ddc74
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-05 00:12:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,5 @@
-// pages/_app.js
-import { useEffect } from "react";
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    (async () => {
-      try {
-        const mod = await import("@farcaster/miniapp-sdk");
-        await mod.sdk.actions.ready();  // tell host we're ready
-      } catch {
-        // Outside Farcaster this is a no-op
-      }
-    })();
-  }, []);
-
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
