b9006f06fb6eb0e1c9e5c3be7c9dac12338e6783
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-06 14:43:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import "../styles/globals.css";
+// pages/_app.js
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { base } from "wagmi/chains";
+
+const { chains, publicClient } = configureChains([base], [publicProvider()]);
+const { connectors } = getDefaultWallets({
+  appName: "SportsHub Betting",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
