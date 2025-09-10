b35d0f8b3a68b80399ebcad7ffc5c0b49e3b5e36
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-10 07:58:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,8 @@
 import "../styles/globals.css";
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
-import { mainnet, base } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import {
   getDefaultWallets,
   RainbowKitProvider
 } from "@rainbow-me/rainbowkit";
-
-import "@rainbow-me/rainbowkit/styles.css";
-
-const { chains, publicClient } = configureChains(
-  [base, mainnet],
-  [publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "SportsHub MiniApp",
-  projectId: "YOUR_WALLETCONNECT_PROJECT_ID", // keep secret in Vercel ENV
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
-
-export default function App({ Component, pageProps }) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
-}
